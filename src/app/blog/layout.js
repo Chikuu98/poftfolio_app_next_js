89357f8cca5f1e2eb68dcad8acf2dac2c2c690de
Chiran Jeewantha <chiran@thesanmark.com>
2023-06-25 12:54:46 +0530

    #3 create layouts

---
+++ src/app/blog/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+        <hi>Hi this is blog</hi>
+        {children}
+    </div>
+  )
+}
+
+export default BlogLayout
