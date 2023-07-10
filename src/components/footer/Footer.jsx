1eea7fffb6579df286f150965dbabf3b7606c1d9
Chiran Jeewantha <chiran@thesanmark.com>
2023-07-10 22:05:20 +0530

    #6 next js images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 chikuhub, All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Chiku Hub"/>
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Chiku Hub"/>
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Chiku Hub"/>
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Chiku Hub"/>
+      </div>
     </div>
   )
 }
