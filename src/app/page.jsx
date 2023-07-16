e946bc7b14687f5cde7c88d8e801e7ad98a9344c
Chiran Jeewantha <chiran@thesanmark.com>
2023-07-16 21:48:25 +0530

    #7 home page design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,17 @@ import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} width={480} height={480} alt=""/>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better Design for your Digital Products</h1>
+        <p className={styles.desc}>
+          Turning your Idea into reality. We bring together the teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} width={480} height={480} alt=""/>
+      </div>
     </div>
   )
 }
